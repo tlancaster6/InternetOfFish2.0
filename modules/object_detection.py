3803f7f7de57001c93b1d15956e72780915cdb45
tlancaster6 <tuckerlancaster@gmail.com>
2023-05-16 16:09:57 -0400

    on-device debugging

--- modules/object_detection.py
+++ modules/object_detection.py
@@ -25,18 +25,22 @@ class DetectorBase:
         self.output_details = self.interpreter.get_output_details()
 
     def detect_yolo(self, img):
-        h, w, ch = img.shape
+        # h, w, ch = img.shape
+        scale = (self.input_size[1] / img.shape[1], self.input_size[0] / img.shape[0])
+        img = cv2.resize(img, self.input_size)
         img = (img / self.scale + self.zero_point).astype(np.uint8)
         img = np.expand_dims(img, axis=0)
         self.interpreter.set_tensor(self.input_details[0]['index'], img)
         self.interpreter.invoke()
-        y = []
-        for output in self.output_details:
-            x = self.interpreter.get_tensor(output['index'])
-            x = (x.astype(np.float32) - self.zero_point) * self.scale  # re-scale
-            y.append(x)
-        y = [x if isinstance(x, np.ndarray) else x.numpy() for x in y]
-        y[0][..., :4] *= [w, h, w, h]
+        dets = detect.get_objects(self.interpreter, self.confidence_thresh, scale)
+        return dets
+        # y = []
+        # for output in self.output_details:
+        #     x = self.interpreter.get_tensor(output['index'])
+        #     x = (x.astype(np.float32) - self.zero_point) * self.scale  # re-scale
+        #     y.append(x)
+        # y = [x if isinstance(x, np.ndarray) else x.numpy() for x in y]
+        # y[0][..., :4] *= [w, h, w, h]
 
     def detect_effdet(self, img):
         scale = (self.input_size[1] / img.shape[1], self.input_size[0] / img.shape[0])
