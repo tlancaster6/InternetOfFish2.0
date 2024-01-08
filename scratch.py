a837d76c4ac9eea3efaff285f48fc9a02d664be0
tlancaster6 <tuckerlancaster@gmail.com>
2024-01-08 13:53:56 -0500

    adding dense detection data generation utility method to benchmarker

--- scratch.py
+++ scratch.py
@@ -2,5 +2,5 @@ from benchmark import BenchMarker
 import pathlib
 
 bm = BenchMarker(model_id='effdet0_fish')
-test_dir = pathlib.Path.home().resolve() / 'valid'
-bm.test_occupancy_accuracy(test_dir)
+parent_dir = pathlib.Path.home() / 'Benchmarking'
+bm.generate_dense_detection_data(parent_dir)
