9b1ffa53268e18673acf4114f3699fd61fa43c38
tlancaster6 <tuckerlancaster@gmail.com>
2024-01-22 11:27:02 -0500

    altering dense detection data generation confidence threshold

--- scratch.py
+++ scratch.py
@@ -2,8 +2,8 @@ from benchmark import ObjectDetectionBenchMarker, BehaviorDetectionBenchmarker
 import pathlib
 
 # bm = ObjectDetectionBenchMarker(model_id='effdet0_fish')
-# parent_dir = pathlib.Path.home() / 'Benchmarking'
-annotated_clip_dir = r"C:\Users\tucke\Benchmarking"
+annotated_clip_dir = pathlib.Path.home() / 'Benchmarking'
+# annotated_clip_dir = r"C:\Users\tucke\Benchmarking"
 behave_bm = BehaviorDetectionBenchmarker(model_id='effdet0_fish', annotated_clip_dir=annotated_clip_dir)
 behave_bm.generate_dense_detection_data()
 behave_bm.generate_data_summary(frame_interval=1)
