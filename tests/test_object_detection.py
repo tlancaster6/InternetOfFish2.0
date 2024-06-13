605b556a0cd63af20a4c9c7fffaa070a386b5087
tlancaster6 <tuckerlancaster@gmail.com>
2024-06-13 14:13:05 -0400

    updating detection tests

--- tests/test_object_detection.py
+++ tests/test_object_detection.py
@@ -3,12 +3,12 @@
 def test_roi_detection(roi_detector_fixture, sample_img_loader_fixture):
     roid, img = roi_detector_fixture, sample_img_loader_fixture
     bbox = roid.detect(img)[0].bbox
-    assert bbox.xmin + bbox.xmax + bbox.ymin + bbox.ymax == 1971
+    assert bbox.xmin + bbox.xmax + bbox.ymin + bbox.ymax == 2039
 
 
 def test_ooi_detection(ooi_detector_fixture, sample_cropped_img_loader_fixture):
     ooid, img = ooi_detector_fixture, sample_cropped_img_loader_fixture
     dets = ooid.detect(img)
     checksum = sum([d.bbox.__getattribute__(att) for d in dets for att in ['xmin', 'ymin', 'xmax', 'ymax']])
-    assert checksum == 1072
+    assert checksum == 1093
 
