f49b95beed58c5ec802d6399b962db7185e0288e
tlancaster <tuckerlancaster@gmail.com>
2024-08-05 12:38:49 -0400

    trying to fix notifier error

--- main.py
+++ main.py
@@ -181,6 +181,7 @@ class Runner:
                 self.behavior_recognizer.append_data(current_datetime.timestamp(), occupancy, thumbnail)
             if current_datetime >= next_behavior_check:
                 if self.behavior_recognizer.check_for_behavior():
+                    logger.info('possible behavioral event. Sending notification')
                     mp4_path = self.video_dir / f'eventclip_{int(current_datetime.timestamp())}.mp4'
                     self.behavior_recognizer.thumbnails_to_mp4(mp4_path)
                     notification = Notification(subject=f'possible behavioral event in {self.config.project_id}',
