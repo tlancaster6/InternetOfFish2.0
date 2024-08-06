3060e19761ab96ce32e38168c8a8057fe7aac154
tlancaster6 <tuckerlancaster@gmail.com>
2024-08-06 11:19:57 -0400

    ensuring re-initialization of cam object at the start of active mode. Adding a temporary test script to quickly test mode cycling

--- modules/behavior_recognition.py
+++ modules/behavior_recognition.py
@@ -26,7 +26,7 @@ class BehaviorRecognizer:
     def append_data(self, timestamp, occupancy, thumbnail):
         self.data_buffer.append((timestamp, occupancy, thumbnail))
         while (len(self.data_buffer) >= 2) and (self.calc_buffer_length_seconds() > self.behavior_check_window):
-            logger.debug(f'buffer too long, popping. length: {self.calc_buffer_length_seconds()}. Max: {self.behavior_check_window}')
+            logger.debug(f'buffer too long, popping. length s: {self.calc_buffer_length_seconds()}. length n: {len(self.data_buffer)}Max: {self.behavior_check_window}')
             self.data_buffer.pop(0)
 
     def calc_activity_fraction(self):
