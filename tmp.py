31139430dfa5312aa6434f3500e986ee49e2d001
tlancaster6 <tuckerlancaster@gmail.com>
2024-08-06 10:24:08 -0400

    ensuring re-initialization of cam object at the start of active mode. Adding a temporary test script to quickly test mode cycling

--- tmp.py
+++ tmp.py
@@ -4,7 +4,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-pid = input('enter pid')
+pid = input('enter pid: ')
 config_path = DEFAULT_DATA_DIR / pid / 'config.yaml'
 runner = Runner(config_path)
 runner.video_split_interval = timedelta(minutes=2)
@@ -14,5 +14,5 @@ for test_iter in range(3):
     logger.info(f'running test iteration {test_iter}')
     runner.end_time = (datetime.now() + mode_switch_interval).time()
     runner.active_mode(round_video_split_time=False)
-    runner.start_time = datetime.now().time()
+    runner.start_time = (datetime.now() + mode_switch_interval).time()
     runner.passive_mode()
